937b44d58adc205736057be62c31f6e890ffc0c3
koekvall <k.o.ekvall@gmail.com>
2023-02-06 15:10:54 -0500

    Add likelihood

--- R/functions.R
+++ R/functions.R
@@ -36,14 +36,22 @@ score_psi <- function(ZtZ, ZtX, Zte, e, H, Psi0, psi0, finf = TRUE)
   B <- A[, 1:r] # This copying can probably be avoided but may not affect speed
   diag(B) <- diag(B) - 1
   B <- crossprod(ZtZ, B)
-  ## H * B should now equal -[ZtZ (I_q - M) H_1, ..., ZtZ (I_q - M) H_r] by
-  ## recycling
-  s_psi[-1] <- s_psi[-1] + (0.5 / psi0) * colSums(matrix(colSums(H * B), nrow = q))
+  ## H * B should now equal -[ZtZ (I_q - M) * H_1, ..., ZtZ (I_q - M) * H_r] by
+  ## recycling, where * denotes elementwise multiplication
+  s_psi[-1] <- s_psi[-1] + (0.5 / psi0) * colSums(matrix(colSums(H * B),
+                                                         nrow = q))
+  return(s_psi)
+}
 
-    if(finf){
-    B <- crossprod(ZtZ, A[, 1:r]) # Re-use storage B
-    I_psi[1, 1] <- (n - 2 * trace_M + sum(diag(crossprod(A[, 1:r], B)))) /
-      psi0^2
-  }
+loglik <- function(ZtZ, Zte, e, Psi0, psi0){
+  n <- length(e)
+  R <- chol(crossprod(Psi0, ZtZ), Imult = 1)
+  two_neg_ll <- n * log(psi0) + sum(log(diag(R)))
+  two_neg_ll <- two_neg_ll + sum(e^2)/psi0
+  # Profiling suggests this is faster than using the cholesky factor
+  two_neg_ll <- two_neg_ll - crossprod(Zte, solve(R, solve(t(R),
+                                            crossprod(Psi0, Zte))))/psi0
+
+  return(-0.5 * two_neg_ll)
 
 }
