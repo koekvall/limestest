569979cf519998cd5e9155606dc9d84d05855475
koekvall <k.o.ekvall@gmail.com>
2023-02-07 14:21:56 -0500

    Start package

--- R/functions.R
+++ R/functions.R
@@ -1,57 +1,58 @@
-score_psi <- function(ZtZ, ZtX, Zte, e, H, Psi0, psi0, finf = TRUE)
+#' @export
+score_psi <- function(Z, ZtZXe, e, H, Psi0, psi0, finf = TRUE)
 {
   # Define dimensions
-  p <- ncol(XtX)
-  q <- ncol(ZtZ)
+  n <- length(e)
+  q <- ncol(Psi0)
   r <- ncol(H) / q # Assumes H = [H_1, ... , H_r], where H_j is q by q
-
+  p <- ncol(ZtZXe) - q - 1
   # Score vector to return
   s_psi <- rep(0, r + 1)
 
   # Fisher information to return
   I_psi <- matrix(NA, r + 1, r + 1)
 
-  # Pre-compute Psi0ZtZ (columns 1:r), Pzi0ZtX (columns (r + 1):(r + p)),
-  # and Psi0Zte (column r + p + 1)
-  A <- crossprod(Psi0, cbind(ZtZ, ZtX, Zte))
+  # Pre-compute Psi0ZtZ (columns 1:q), Pzi0ZtX (columns (q + 1):(q + p)),
+  # and Psi0Zte (column q + p + 1)
+  A <- Matrix::crossprod(Psi0, ZtZXe)
 
   # MAIN COMPUTATION? PROFILE
-  # Sparse solve needed for all quantities
-  B <- Psi0ZtZXe[, 1:r] # = Psi0ZtZ
-  diag(B) <- diag(B) + 1
-  A <- solve(B, A)   # Matrix denoted M in manuscript is A[, 1:r]
+  # Matrix denoted M in manuscript is A[, 1:q]
+  A <- Matrix::solve(A[, 1:q] + Matrix::Diagonal(q), A, sparse = TRUE)
 
   # Score for error variance psi_0
-  e <- (e - Z %*% A[, p + r + 1]) / psi0 # Replace residual vector
-  trace_M <- sum(diag(A[, 1:r]))
-  s_psi[1] <- (sum(e^2) - (n - trace_M) / psi0) / 2
+  # NB: REPLACE e by Sigma^{-1}e
+  e <- (1 / psi0) * (e - Z %*% A[, q + p + 1]) # = Sigma^{-1}e
+  trace_M <- sum(Matrix::diag(A[, 1:q]))
+  s_psi[1] <- 0.5 * sum(e^2) - (0.5 / psi0) * (n - trace_M)
 
-  # Score for parameter vector psi
-  v = crossprod(Z, e)
+  v <- Matrix::crossprod(Z, e)
+  # Use recycling to compute v'H_i v for all Hi.
+  s_psi[-1] <- 0.5 * colSums(matrix(as.vector(Matrix::crossprod(v, H)), nrow = q) * v)
 
-  ## the right-most elementwise multiplication uses recycling; make sure correct
-  ## assumes H = [H_1, ..., H_r]
-  s_psi[-1] <- 0.5 * colSums(matrix(as.vector(crossprod(v, H)), nrow = q) * v)
-
-  B <- A[, 1:r] # This copying can probably be avoided but may not affect speed
-  diag(B) <- diag(B) - 1
-  B <- crossprod(ZtZ, B)
-  ## H * B should now equal -[ZtZ (I_q - M) * H_1, ..., ZtZ (I_q - M) * H_r] by
+  B <- A[, 1:q] - Matrix::Diagonal(q)
+  B <- Matrix::crossprod(ZtZXe[, 1:q], B)
+  dim(B) <- c(prod(dim(B)), 1)
+  ## Below should equal -[ZtZ (I_q - M) * H_1, ..., ZtZ (I_q - M) * H_r] by
   ## recycling, where * denotes elementwise multiplication
-  s_psi[-1] <- s_psi[-1] + (0.5 / psi0) * colSums(matrix(colSums(H * B),
-                                                         nrow = q))
+  H <- H * Matrix::sparseVector(x = B@x, i = B@i + 1, length = dim(B)[1])
+  s_psi[-1] <- s_psi[-1] + (0.5 / psi0) * colSums(matrix(Matrix::colSums(H), nrow = q))
   return(s_psi)
 }
-
+#' @export
 loglik <- function(ZtZ, Zte, e, Psi0, psi0){
   n <- length(e)
-  R <- chol(crossprod(Psi0, ZtZ), Imult = 1)
-  two_neg_ll <- n * log(psi0) + sum(log(diag(R)))
+  B <- Matrix::crossprod(Psi0, ZtZ) + Matrix::Diagonal(ncol(ZtZ))
+  two_neg_ll <- n * log(psi0) + Matrix::determinant(B, logarithm = TRUE)$modulus
   two_neg_ll <- two_neg_ll + sum(e^2)/psi0
-  # Profiling suggests this is faster than using the cholesky factor
-  two_neg_ll <- two_neg_ll - crossprod(Zte, solve(R, solve(t(R),
-                                            crossprod(Psi0, Zte))))/psi0
 
-  return(-0.5 * two_neg_ll)
+  # Profiling suggests this is faster than using
+  # R <- Matrix::Cholesky(B) and then solve(R, ...) here
+  two_neg_ll <- two_neg_ll - Matrix::crossprod(Zte, Matrix::solve(B,
+                                            Matrix::crossprod(Psi0, Zte))) / psi0
+
+  return(-0.5 * as.vector(two_neg_ll))
 
 }
+
+
--- R/test.R
+++ R/test.R
@@ -1,5 +1,6 @@
 library(foreign)
 library(lme4)
+library(Matrix)
 fev1_dat <- read.dta("http://www.hsph.harvard.edu/fitzmaur/ala2e/fev1.dta")
 fit <- lmer(exp(logfev1) ~ age + ht + baseage + baseht + (age|id),
             data = fev1_dat, REML = FALSE)
@@ -10,4 +11,16 @@ y <- getME(fit, "y")
 
 random_effects <- ranef(fit, condVar = TRUE)
 cov_matrices <- attr(random_effects[[1]], "postVar")
+H1 <- Matrix::kronecker(Matrix::Diagonal(300), matrix(c(1, 0, 0, 0), 2, 2))
+H2 <- Matrix::kronecker(Matrix::Diagonal(300), matrix(c(0, 1, 1, 0), 2, 2))
+H3 <- Matrix::kronecker(Matrix::Diagonal(300), matrix(c(0, 0, 0, 1), 2, 2))
+Psi_hat <- Matrix::kronecker(Matrix::Diagonal(300), matrix(VarCorr(fit)$id, 2, 2))
+psi0_hat <- attr(VarCorr(fit), "sc")^2
+beta_hat <- fixef(fit)
+e <- y - X %*% beta_hat
+ZtZXe <- Matrix::crossprod(Z, cbind(Z, X, e))
 
+
+#loglik(Z = Z, ZtZXe = ZtZXe, e = e, Psi0 = Psi_hat / psi0_hat, psi0 = psi0_hat);
+score_psi(Z = Z, ZtZXe = ZtZXe, e = e, H = cbind(H1, H2, H3),
+          Psi0 = Psi_hat/psi0_hat, psi0 = psi0_hat, FALSE)
