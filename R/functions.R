e5df2ba481d46f8d87c933ee50fcc850180bfb4f
koekvall <k.o.ekvall@gmail.com>
2023-02-09 09:16:23 -0500

    Work on information matrix

--- R/functions.R
+++ R/functions.R
@@ -27,27 +27,38 @@ score_psi <- function(Z, ZtZXe, e, H, Psi0, psi0, finf = TRUE)
 
 
   # Use recycling to compute v'H_i v for all Hi
-  v <- as.vector(Matrix::crossprod(Z, e)) # sparse matrix does not recycle
-  s_psi[-1] <- 0.5 * colSums(matrix(as.vector(Matrix::crossprod(v, H)), nrow = q) * v)
+  v <- as(Matrix::crossprod(Z, e), "sparseVector") # sparse matrix does not recycle
+  s_psi[-1] <- 0.5 * colSums(matrix(as.vector(Matrix::crossprod(v, H)), nrow = q) * as.vector(v))
 
   B <- A[, 1:q] - Matrix::Diagonal(q)
   B <- Matrix::crossprod(ZtZXe[, 1:q], B)
 
   # This wastes memory; done to use recycling of b in next step, which does
   # not currently work with B
-  dim(B) <- c(prod(dim(B)), 1)
-  b <- Matrix::sparseVector(x = B@x, i = B@i + 1, length = dim(B)[1])
 
   ## Below should equal -[ZtZ (I_q - M) * H_1, ..., ZtZ (I_q - M) * H_r] by
   ## recycling, where * denotes elementwise multiplication
-  H <- H * b
+  H <- H * as(B, "sparseVector")
   s_psi[-1] <- s_psi[-1] + (0.5 / psi0) * colSums(matrix(Matrix::colSums(H), nrow = q))
-  return(s_psi)
 
-  # May be avoided by never changing dim(B) above
-  dim(B) <- c(q, q)
-  I_psi[1, 1] <- (1 / psi0^2) / (n - 2 * trace_M + sum(t(A[, 1:q]) *
-                                Matrix::crossprod(ZtZXe[, 1:q], A[, 1:q])))
+  if(finf){
+    I_psi[1, 1] <- (1 / psi0^2) / (n - 2 * trace_M + sum(t(A[, 1:q]) *
+                                                           Matrix::crossprod(ZtZXe[, 1:q], A[, 1:q])))
+
+    D <- as(as(A[, 1:q] - Matrix::Diagonal(q), "sparseVector") *
+              as(B, "sparseVector"), "sparseMatrix")
+    dim(D) <- c(nrow(D) / r, r)
+    I_psi[1, -1] <- Matrix::colSums(D)
+
+    for(ii in 1:r){
+      col_idx_ii <- ((ii - 1) * q + 1):(ii * q)
+      for(jj in ii:r){
+        col_idx_jj <- ((jj - 1) * q + 1):(jj * q)
+        I[ii, jj] <- sum(H[, row_idx] * H[, col_idx]) / psi0^2
+      }
+    }
+
+  }
 
 
 }
