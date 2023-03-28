aa97f3b8a0c34c28bfd3e0e98ca7b512e32f1275
koekvall <k.o.ekvall@gmail.com>
2023-03-28 17:09:43 -0400

    Discover problem with finf for psi0

--- test/simulation_restricted.R
+++ test/simulation_restricted.R
@@ -1,11 +1,12 @@
 library(Matrix)
-test_mc_sigma_crossed <- function(psi_test){
+library(doParallel)
+test_mc_sigma_crossed <- function(psi_test = c(1, 0, 1), num_reps = 1e3){
   m <- 20 # Number of units
   Z <- as(cbind(Matrix::kronecker(Matrix::diag(1, nrow = m), matrix(1, m, 1)),
                 Matrix::kronecker(matrix(1, m, 1), Matrix::diag(1, nrow = m))),
           "sparseMatrix")
   X <- cbind(1, matrix(rnorm(m^2), m^2, 1))
-  b <- rep(0, ncol(X))
+  b <- runif(ncol(X), -1, 1)
   Psi1 <- diag(psi_test[-1], 2)
   psi0 <- psi_test[1]
   Psi <- Matrix::kronecker(Psi1, Matrix::diag(1, m))
@@ -13,12 +14,7 @@ test_mc_sigma_crossed <- function(psi_test){
   H1 <- Matrix::kronecker(diag(c(1, 0), 2), Matrix::diag(1, m))
   H2 <- Matrix::kronecker(diag(c(0, 1), 2), Matrix::diag(1, m))
   H <- as(cbind(H1, H2), "sparseMatrix")
-
-  #Psi1_eig <- eigen(Psi1)
-  #R1 <- tcrossprod(diag(sqrt(Psi1_eig$values), ncol(Psi1)), Psi1_eig$vectors)
   R <-  as(diag(rep(sqrt(psi_test[-1]), each = m)), "sparseMatrix")
-
-  num_reps <- 1e3
   one_sim_score <- function(seed){
     set.seed(seed)
     y <- rnorm(nrow(Z), sd = sqrt(psi0)) + Z %*% crossprod(R, rnorm(ncol(R)))
@@ -39,29 +35,36 @@ test_mc_sigma_crossed <- function(psi_test){
                       lik = TRUE)$score
 
   }
-
+  cl <- makeCluster(8)
+  registerDoParallel(cl)
+  out <- foreach(ii=1:num_reps, .combine = rbind, .errorhandling = "remove") %dopar% one_sim_score(ii)
+  stopCluster(cl)
+  MC_inf<- cov(out)
+  y_outside <- rep(0, nrow(Z))
+  AN_inf <- limestest::res_ll(XtX = crossprod(X),
+                              XtY = crossprod(X, y_outside),
+                              XtZ = crossprod(X, Z),
+                              ZtZ = crossprod(Z),
+                              YtZ = crossprod(y_outside, Z),
+                              Y = y_outside,
+                              X = X,
+                              Z = Z,
+                              H = H,
+                              Psi0 = Psi0,
+                              psi0 = psi0,
+                              score = TRUE,
+                              finf = TRUE,
+                              lik = TRUE)$finf
+  cat("MC information: \n")
+  print(MC_inf)
+  cat("Analytical information: \n")
+  print(as.matrix(AN_inf))
 }
 
+test_mc_sigma_crossed(psi_test = c(1, 1, 0))
 
 
-out <- sapply(1:num_reps, one_sim_score)
-MC_inf<- cov(t(out))
-AN_inf <- limestest::res_ll(XtX = crossprod(X),
-                           XtY = crossprod(X, y_outside),
-                           XtZ = crossprod(X, Z),
-                           ZtZ = crossprod(Z),
-                           YtZ = crossprod(y_outside, Z),
-                           Y = y_outside,
-                           X = X,
-                           Z = Z,
-                           H = H,
-                           Psi0 = Psi0,
-                           psi0 = psi0,
-                           score = TRUE,
-                           finf = TRUE,
-                           lik = TRUE)$finf
 
-MC_inf
-AN_inf
+
 
 
