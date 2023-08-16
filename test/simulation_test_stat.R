1a7ec335cedb30b4a6de21b5148d6f6318cad398
koekvall <k.o.ekvall@gmail.com>
2023-08-16 14:51:37 -0400

    Work on simulations

--- test/simulation_test_stat.R
+++ test/simulation_test_stat.R
@@ -5,14 +5,18 @@ num_reps <- 1e3
 ################################################################################
 ## INDEPENDENT CLUSTERS ########################################################
 ################################################################################
-num_cluster <- 40
+
+## SET PARAMETERS ##############################################################
+num_cluster <- 20
 num_ind <- 5
-Z <- Matrix::bdiag(replicate(num_cluster, cbind(1, rnorm(num_ind)),
-                             simplify = FALSE))
-X <- matrix(rnorm(nrow(Z) * 20), nrow = nrow(Z), ncol = 20)
-b <- runif(ncol(X))
-Psi1 <- matrix(c(0.1, 0, 0, 0.1), 2, 2)
+p <- 2
+Psi1 <- matrix(c(1, -0.99, -0.99, 1), 2, 2)
 psi0 <- 1
+################################################################################
+n <- num_cluster * num_ind
+X <- cbind(1, matrix(runif(n * p, min = -1, max = 1), nrow = n, ncol = p))
+Z <- Matrix::bdiag(lapply(1:num_cluster, function(ii)X[((ii - 1) * num_ind + 1):(ii * num_ind), 1:2]))
+b <- rnorm(ncol(X))
 Psi <- Matrix::kronecker(Matrix::Diagonal(num_cluster), Psi1)
 Psi0 <- Matrix::kronecker(Matrix::Diagonal(num_cluster), Psi1 / psi0)
 H1 <- Matrix::kronecker(Matrix::Diagonal(num_cluster), matrix(c(1, 0, 0, 0), 2, 2))
@@ -28,6 +32,10 @@ XtX = crossprod(X)
 XtZ = crossprod(X, Z)
 ZtZ = crossprod(Z)
 Xb <- X %*% b
+
+Sigma <- diag(psi0, nrow(X)) + Z %*% tcrossprod(Psi, Z)
+M <- solve(Sigma, X)
+G <- solve(crossprod(X, M), t(M))
 
 one_sim_test <- function(seed){
   set.seed(seed)
@@ -62,20 +70,49 @@ one_sim_test <- function(seed){
   test_stat_REML <- as.vector(crossprod(stuff_REML$score,
                                       solve(stuff_REML$finf, stuff_REML$score)))
 
-  c(pchisq(test_stat, df = 4, lower = F),
-    pchisq(test_stat_REML, df = 4, lower = F))
+  mc_data_frame <- tidyr::tibble("out" = as.vector(y),
+                                 "clust" = as.factor(rep(1:num_cluster,
+                                                         each = num_ind)))
+
+  mc_data_frame <- dplyr::bind_cols(mc_data_frame, tidyr::as_tibble(X[, -1]))
+  fit <- lme4::lmer(out ~ . - clust + (1 + V1|clust), data = mc_data_frame,
+                    REML = F)
+
+  beta_null <- G %*% y
+  ll_null <- mvtnorm::dmvnorm(x = as.vector(y),
+                              mean = as.vector(X %*% beta_null),
+                              sigma = as.matrix(Sigma), log = TRUE)
+
+  beta_mle <- lme4::fixef(fit)
+  VC <- as.data.frame(lme4::VarCorr(fit))
+  Psi1_mle <- matrix(c(VC$vcov[1], VC$vcov[3], VC$vcov[3], VC$vcov[2]), 2, 2)
+  psi0_mle <- VC$vcov[4]
+  Psi_mle <- Matrix::kronecker(Matrix::Diagonal(num_cluster), Psi1_mle)
+  Sigma_mle <- diag(psi0_mle, nrow(X)) + Z %*% tcrossprod(Psi_mle, Z)
+  ll_mle <- mvtnorm::dmvnorm(x = as.vector(y),
+                             mean = as.vector(X %*% beta_mle),
+                             sigma = as.matrix(Sigma_mle), log = TRUE)
+  test_stat_lrt <- 2 * (ll_mle - ll_null)
+
+
+  c(test_stat, test_stat_REML, test_stat_lrt)
 
 }
 cl <- makeCluster(6)
 registerDoParallel(cl)
-p_vals <- foreach(ii=1:1e3, .combine = rbind, .errorhandling = "remove") %dopar% one_sim_test(ii)
-print(colMeans(p_vals < 0.05))
-par(mfrow = c(1, 2))
-plot(seq(0.01, 0.99, 0.01), quantile(p_vals[, 1], seq(0.01, 0.99, 0.01)), xlab =
-       "Uniform quantile", ylab = "Empirical quantile", main = "LL")
+test_stats <- foreach(ii=1:1e3, .combine = rbind, .errorhandling = "remove") %dopar% one_sim_test(ii)
+print(colMeans(test_stats > qchisq(0.95, df = 4)))
+
+ppoints <- seq(0.01, 0.99, 0.01)
+par(mfrow = c(1, 3))
+plot(qchisq(ppoints, df = 4), quantile(test_stats[, 1], ppoints), xlab =
+       "Chi-square (4) quantiles", ylab = "Empirical quantile", main = "LL")
 abline(a = 0, b = 1, lwd = 2)
-plot(seq(0.01, 0.99, 0.01), quantile(p_vals[, 2], seq(0.01, 0.99, 0.01)), xlab =
-       "Uniform quantile", ylab = "Empirical quantile", main = "RLL")
+plot(qchisq(ppoints, df = 4), quantile(test_stats[, 2], ppoints), xlab =
+       "Chi-square (4) quantiles", ylab = "Empirical quantile", main = "RLL")
+abline(a = 0, b = 1, lwd = 2)
+plot(qchisq(ppoints, df = 4), quantile(test_stats[, 3], ppoints), xlab =
+       "Chi-square (4) quantiles", ylab = "Empirical quantile", main = "LRT")
 abline(a = 0, b = 1, lwd = 2)
 stopCluster(cl)
 
@@ -83,22 +120,27 @@ stopCluster(cl)
 ################################################################################
 ## CROSSED RANDOM EFFECTS ######################################################
 ################################################################################
-m <- 20 # Number of units
-Z <- as(cbind(Matrix::kronecker(Matrix::diag(1, nrow = m), matrix(1, m, 1)),
-           Matrix::kronecker(matrix(1, m, 1), Matrix::diag(1, nrow = m))), "sparseMatrix")
-X <- cbind(1, matrix(rnorm(m^2), m^2, 1))
-b <- rep(0, ncol(X))
-Psi1 <- matrix(c(0.1, 0, 0, 0.1), 2, 2)
-psi0 <- 1
-Psi <- Matrix::kronecker(Psi1, Matrix::diag(1, m))
-Psi0 <- Matrix::kronecker(Psi1 / psi0, Matrix::diag(1, m))
-H1 <- Matrix::kronecker(diag(c(1, 0), 2), Matrix::diag(1, m))
-H2 <- Matrix::kronecker(diag(c(0, 1), 2), Matrix::diag(1, m))
-H <- as(cbind(H1, H2), "sparseMatrix")
 
-#Psi1_eig <- eigen(Psi1)
-#R1 <- tcrossprod(diag(sqrt(Psi1_eig$values), ncol(Psi1)), Psi1_eig$vectors)
-R <-  as(Matrix::kronecker(diag(sqrt(diag(Psi1))), Matrix::diag(1, m)), "sparseMatrix")
+## SET PARAMETERS ##############################################################
+n1 <- 20
+n2 <- 10
+p <- 10
+psi1 <- c(0.1, 1)
+psi0 <- 1
+################################################################################
+
+Z <- as(cbind(Matrix::kronecker(Matrix::diag(1, nrow = n1), matrix(1, n2, 1)),
+           Matrix::kronecker(matrix(1, n1, 1), Matrix::diag(1, nrow = n2))),
+        "sparseMatrix")
+X <- cbind(1, matrix(runif(n1 * n2 * p, min = -1, max = 1), n1 * n2, p))
+b <- rep(0, ncol(X))
+
+Psi <- Matrix::Diagonal(n1 + n2, c(rep(psi1[1], n1), rep(psi1[2], n2)))
+Psi0 <- Psi / psi0
+H1 <- Matrix::Diagonal(n1 + n2, c(rep(1, n1), rep(0, n2)))
+H2 <- Matrix::Diagonal(n1 + n2, c(rep(0, n1), rep(1, n2)))
+H <- as(cbind(H1, H2), "sparseMatrix")
+R <- sqrt(Psi)
 
 XtX = crossprod(X)
 XtZ = crossprod(X, Z)
