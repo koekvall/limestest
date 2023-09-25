3ec9f9f3b4905482924bdd661923da557f110b04
koekvall <k.o.ekvall@gmail.com>
2023-09-25 13:35:05 -0400

    Update test.R

--- test/test.R
+++ test/test.R
@@ -75,7 +75,7 @@ analytical_score <- loglik_psi(Z = Z, ZtZXe = ZtZXe, e = e, H = H,
 numerical_hess <- numDeriv::hessian(loglik_test, c(psi0_test, Psi_test[1, 1],
                                                    Psi_test[2, 1], Psi_test[2, 2]))
 analytical_hess <- -loglik_psi(Z = Z, ZtZXe = ZtZXe, e = e, H = H,
-                               Psi0 = Psi_test/psi0_test, psi0 = psi0_test, loglik = F,
+                               Psi0 = Psi_test / psi0_test, psi0 = psi0_test, loglik = F,
                                score = T, finf = T, expected = F)$finf
 
 cat("The max absolute difference between numerical and analytical score at random point is: ",
@@ -91,3 +91,56 @@ cat("The max relative difference between numerical and analytical Hessian at ran
     max(abs(numerical_hess - analytical_hess) / numerical_hess), "\n")
 
 
+# Simulation for Hessian
+# n_reps <- 1e4
+# n1 <- 20
+# n2 <- 3
+# n <- n1 * n2
+# p <- 1
+# X <- matrix(runif(n * p, min = -1, max = 1), nrow = n, ncol = p)
+# Z <- Matrix::bdiag(replicate(n1, cbind(1, rnorm(n2)),
+#                                   simplify = FALSE))
+# ZtZ <- crossprod(Z)
+# ZtX <- crossprod(Z, X)
+# psi0_star <- 0.1
+# Psi1_star <- matrix(c(0.5, 0.2, 0.2, 0.5), 2, 2)
+# Psi_star <- Matrix::kronecker(Matrix::Diagonal(n1), Psi1_star)
+# Psi0_star <- Psi_star / psi0_star
+# R_star <- Matrix::kronecker(Matrix::Diagonal(n1), chol(Psi1_star))
+# H1 <- Matrix::kronecker(Matrix::Diagonal(n1), matrix(c(1, 0, 0, 0), ncol = 2,nrow =  2))
+# H2 <- Matrix::kronecker(Matrix::Diagonal(n1), matrix(c(0, 1, 1, 0), ncol = 2,nrow =  2))
+# H3 <- Matrix::kronecker(Matrix::Diagonal(n1), matrix(c(0, 0, 0, 1), ncol = 2,nrow =  2))
+# H <- cbind(H1, H2, H3)
+#
+# hess_list <- list()
+# y_list <- list()
+# for(ii in 1:n_reps){
+#   y <- rnorm(nrow(Z), sd = sqrt(psi0_star)) +
+#     Z %*% crossprod(R_star,  rnorm(ncol(R_star))) # beta_star = 0
+#   Zte <- crossprod(Z, y)
+#   hess_list[[ii]]<- limestest::loglik_psi(Z = Z,
+#                         ZtZXe = cbind(ZtZ, ZtX, Zte),
+#                         e = y,
+#                         H = H,
+#                         Psi0 = Psi0_star,
+#                         psi0 = psi0_star,
+#                         loglik = FALSE,
+#                         score = FALSE,
+#                         finf = TRUE,
+#                         expected = FALSE)$finf
+#   y_list[[ii]] <- y
+# }
+#
+# MC_inf <- Reduce("+", hess_list) / length(hess_list)
+# analytical_inf <- limestest::loglik_psi(Z = Z,
+#                                         ZtZXe = cbind(ZtZ, ZtX, Zte),
+#                                         e = y,
+#                                         H = H,
+#                                         Psi0 = Psi0_star,
+#                                         psi0 = psi0_star,
+#                                         loglik = FALSE,
+#                                         score = FALSE,
+#                                         finf = TRUE,
+#                                         expected = TRUE)$finf
+#
+#
